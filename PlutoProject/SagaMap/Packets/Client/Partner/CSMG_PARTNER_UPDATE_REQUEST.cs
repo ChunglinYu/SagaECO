c9d2ba60179708b1af29d1d19adcf3fa84fb4649
ChunglinYu <chunglin.net@gmail.com>
2024-11-24 17:54:10 +0800

    fix: 修正 CSMG_PARTNER_UPDATE_REQUEST 未返回資訊問題。
    未確實回應 Partner 資訊，造成 Partner 能力顯示不正常。

--- PlutoProject/SagaMap/Packets/Client/Partner/CSMG_PARTNER_UPDATE_REQUEST.cs
+++ PlutoProject/SagaMap/Packets/Client/Partner/CSMG_PARTNER_UPDATE_REQUEST.cs
@@ -47,7 +47,7 @@ namespace SagaMap.Packets.Client
 
         public override void Parse(SagaLib.Client client)
         {
-            //((MapClient)(client)).netIO.Disconnect();
+            ((MapClient)(client)).SendPetDetailInfo();
         }
 
     }
